91cc32f73f7ab5678e13761903a9c813c420ea57
Radicorp <unduhdrive@gmail.com>
2021-01-03 00:37:47 +0800

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask Me Anyting'),
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int jawabanKamu = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            jawabanKamu = Random().nextInt(5) + 1;
+          });
+        },
+        child: Center(
+          child: Image.asset('images/ball$jawabanKamu.png'),
+        ),
+      ),
+    );
+  }
+}
